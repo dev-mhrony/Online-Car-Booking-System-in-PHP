d89a7f1d501ebbb20a1a8e853447734628843cc4
dev-mhrony <mh.rony.dhanvi@gmail.com>
2023-01-27 16:02:25 +0600

    Last Update - 27/01/2023

--- DATABASE FILE/OnlineCarBooking.sql
+++ DATABASE FILE/OnlineCarBooking.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 14, 2022 at 07:55 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Host: localhost:3306
+-- Generation Time: Jan 27, 2023 at 10:00 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `vehiclebookings`
+-- Database: `carbooking`
 --
 
 -- --------------------------------------------------------
@@ -27,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tms_admin` (
-  `a_id` int(20) NOT NULL,
+  `a_id` int NOT NULL,
   `a_name` varchar(200) NOT NULL,
   `a_email` varchar(200) NOT NULL,
   `a_pwd` varchar(200) NOT NULL
@@ -38,7 +39,7 @@ CREATE TABLE `tms_admin` (
 --
 
 INSERT INTO `tms_admin` (`a_id`, `a_name`, `a_email`, `a_pwd`) VALUES
-(1, 'System Admin', 'admin@mail.com', 'codeastro.com');
+(3, 'Code Camp BD', 'admin@gmail.com', '0d89ec971a7bcfe26d68c177a9d53334');
 
 -- --------------------------------------------------------
 
@@ -47,7 +48,7 @@ INSERT INTO `tms_admin` (`a_id`, `a_name`, `a_email`, `a_pwd`) VALUES
 --
 
 CREATE TABLE `tms_feedback` (
-  `f_id` int(20) NOT NULL,
+  `f_id` int NOT NULL,
   `f_uname` varchar(200) NOT NULL,
   `f_content` longtext NOT NULL,
   `f_status` varchar(200) NOT NULL
@@ -69,7 +70,7 @@ INSERT INTO `tms_feedback` (`f_id`, `f_uname`, `f_content`, `f_status`) VALUES
 --
 
 CREATE TABLE `tms_pwd_resets` (
-  `r_id` int(20) NOT NULL,
+  `r_id` int NOT NULL,
   `r_email` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -78,7 +79,7 @@ CREATE TABLE `tms_pwd_resets` (
 --
 
 INSERT INTO `tms_pwd_resets` (`r_id`, `r_email`) VALUES
-(2, 'admin@mail.com');
+(2, 'admin@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -87,7 +88,7 @@ INSERT INTO `tms_pwd_resets` (`r_id`, `r_email`) VALUES
 --
 
 CREATE TABLE `tms_syslogs` (
-  `l_id` int(20) NOT NULL,
+  `l_id` int NOT NULL,
   `u_id` varchar(200) NOT NULL,
   `u_email` varchar(200) NOT NULL,
   `u_ip` varbinary(200) NOT NULL,
@@ -103,14 +104,14 @@ CREATE TABLE `tms_syslogs` (
 --
 
 CREATE TABLE `tms_user` (
-  `u_id` int(20) NOT NULL,
+  `u_id` int NOT NULL,
   `u_fname` varchar(200) NOT NULL,
   `u_lname` varchar(200) NOT NULL,
   `u_phone` varchar(200) NOT NULL,
   `u_addr` varchar(200) NOT NULL,
   `u_category` varchar(200) NOT NULL,
   `u_email` varchar(200) NOT NULL,
-  `u_pwd` varchar(20) NOT NULL,
+  `u_pwd` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `u_car_type` varchar(200) NOT NULL,
   `u_car_regno` varchar(200) NOT NULL,
   `u_car_bookdate` varchar(200) NOT NULL,
@@ -122,16 +123,7 @@ CREATE TABLE `tms_user` (
 --
 
 INSERT INTO `tms_user` (`u_id`, `u_fname`, `u_lname`, `u_phone`, `u_addr`, `u_category`, `u_email`, `u_pwd`, `u_car_type`, `u_car_regno`, `u_car_bookdate`, `u_car_book_status`) VALUES
-(3, 'Demo', 'User', '070678909', '90100 Machakos ', 'Driver', 'demouser@tms.com', 'demo123', 'SUV', 'CA1001', '2022-09-01', 'Approved'),
-(4, 'John', 'Settles', '7145698540', '45 Clearview Drive', 'Driver', 'johns@mail.com', 'password', '', '', '', ''),
-(5, 'Joseph', 'Yung', '7896587777', '72 Doe Meadow Drive', 'Driver', 'joseph@mail.com', 'password', '', '', '', ''),
-(6, 'Vincent', 'Pelletier', '4580001456', '58 Farland Avenue', 'Driver', 'vincentp@mail.com', 'password', '', '', '', ''),
-(7, 'Jesse', 'Robinson', '1458887855', '73 Fleming Way', 'Driver', 'jesser@mail.com', 'password', '', '', '', ''),
-(8, 'Nelson', 'Ford', '7458965874', '58 West Side Avenue', 'User', 'nelford@mail.com', 'password', 'Sedan', 'CA1690', '2022-09-13', 'Approved'),
-(9, 'Paul', 'Mills', '7412563258', '12 Red Maple Drive', 'User', 'paul@mail.com', 'password', 'Sedan', 'CA2077', '2022-09-14', 'Pending'),
-(10, 'Liam', 'Moore', '7410001212', '114 Bleck Street', 'User', 'liamoore@mail.com', 'password', 'Sedan', 'CA1690', '2022-09-14', 'Approved'),
-(11, 'Jeff', 'Lewis', '7854545454', '114 Test Adr', 'User', 'jeff@mail.com', 'password', 'Sedan', 'CA7700', '2022-09-14', 'Pending'),
-(12, 'Kenya', 'Norman', '7896547855', '114 Test Addr', 'User', 'normank@mail.com', 'password', 'Bus', 'CA7766', '2022-09-15', 'Pending');
+(13, 'Clint', '01', '01600000000', 'Bogura,Bangladesh', 'User', 'clint@gmail.com', '123456', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -140,7 +132,7 @@ INSERT INTO `tms_user` (`u_id`, `u_fname`, `u_lname`, `u_phone`, `u_addr`, `u_ca
 --
 
 CREATE TABLE `tms_vehicle` (
-  `v_id` int(20) NOT NULL,
+  `v_id` int NOT NULL,
   `v_name` varchar(200) NOT NULL,
   `v_reg_no` varchar(200) NOT NULL,
   `v_pass_no` varchar(200) NOT NULL,
@@ -209,32 +201,39 @@ ALTER TABLE `tms_vehicle`
 -- AUTO_INCREMENT for table `tms_admin`
 --
 ALTER TABLE `tms_admin`
-  MODIFY `a_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `a_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `tms_feedback`
 --
 ALTER TABLE `tms_feedback`
-  MODIFY `f_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `f_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `tms_pwd_resets`
 --
 ALTER TABLE `tms_pwd_resets`
-  MODIFY `r_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `r_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `tms_syslogs`
 --
 ALTER TABLE `tms_syslogs`
-  MODIFY `l_id` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `l_id` int NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `tms_user`
 --
 ALTER TABLE `tms_user`
-  MODIFY `u_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `u_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
 --
 -- AUTO_INCREMENT for table `tms_vehicle`
 --
 ALTER TABLE `tms_vehicle`
-  MODIFY `v_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `v_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
